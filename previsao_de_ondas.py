df458f027bbcb4f41b773bbb339c79ce281c0dff
Juliano <julianocf.2009@gmail.com>
2020-04-01 20:43:00 -0300

    Reestruturação do código

--- previsao_de_ondas.py
+++ previsao_de_ondas.py
@@ -1,20 +1,18 @@
 import requests
 import xml.dom.minidom as minidom
 
-from previsao_do_tempo import PrevisaoDoTempo
+from previsao import Previsao
 
 class PrevisaoDeOndas:
     def __init__( self, nome_da_cidade ):
         self._previsao = ""
-        self._codigo_da_cidade = PrevisaoDoTempo(nome_da_cidade)._obter_codigo_da_cidade()
+        self._codigo_da_cidade = Previsao().obter_codigo_da_cidade(nome_da_cidade)
 
     def _obter_previsao_do_dia( self ):
         
         if self._codigo_da_cidade:
 
             retorno_requisicao = requests.get( "http://servicos.cptec.inpe.br/XML/cidade/" + self._codigo_da_cidade + "/dia/0/ondas.xml" )
-
-            print("http://servicos.cptec.inpe.br/XML/cidade/" + self._codigo_da_cidade + "/dia/0/ondas.xml")
 
             if "undefined" in retorno_requisicao.text:
                 print( "Falha ao obter a previsão!" )
@@ -34,9 +32,46 @@ class PrevisaoDeOndas:
 
             return minidom.parseString(retorno_requisicao.text)
 
+    def _converter_previsao_em_dicionario(self, previsao_xml):
+        
+        previsao_dict = {}
+
+        if previsao_xml:
+
+            previsao_dict["cabecalho"] = [] 
+            previsao_dict["linhas"] = []
+
+            previsao_lista = previsao_xml.getElementsByTagName("cidade").item(0).childNodes        
+            primeira_iteracao = True
+            for previsao in previsao_lista:
+                if previsao.tagName == "nome":
+                    previsao_dict["cidade"] =  previsao.firstChild.data
+                elif previsao.tagName == "uf":
+                    previsao_dict["uf"] =  previsao.firstChild.data
+                elif previsao.tagName == "atualizacao":
+                    previsao_dict["data_da_consulta"] =  previsao.firstChild.data
+                else:
+                    if previsao.hasChildNodes():
+                        previsao_filhos = previsao.childNodes
+                        linha = []
+                        for elemento in previsao_filhos:
+                            if primeira_iteracao:
+                                previsao_dict["cabecalho"].append(elemento.tagName)
+                            
+                            if elemento.tagName == "direcao":
+                                linha.append( self._converter_sigla_do_ponto_cardeal( elemento.firstChild.data ) )
+                            elif elemento.tagName == "vento_dir":
+                                linha.append( self._converter_sigla_do_ponto_cardeal( elemento.firstChild.data ) )
+                            else:
+                                linha.append( elemento.firstChild.data )
+                        previsao_dict["linhas"].append( linha )
+                        primeira_iteracao = False
+        
+        return previsao_dict
+
     def _converter_sigla_do_ponto_cardeal(self, sigla):
 
-        previsao = {
+        ponto_cardeal = {
             "N": "Norte",
             "NNE": "Nor-Nordeste",
             "NE": "Nordeste",
@@ -55,4 +90,4 @@ class PrevisaoDeOndas:
             "NNW": "Noroeste"
         }
 
-        return previsao.get(sigla, "Sigla não encontrada.")
+        return ponto_cardeal.get(sigla, "Sigla não encontrada.")
