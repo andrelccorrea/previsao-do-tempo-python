992a339d5dd265c87600549ccf51eb4bff145b53
Juliano <julianocf.2009@gmail.com>
2020-04-01 15:35:53 -0300

    Exibição da previsão de ondas

--- previsao_de_ondas.py
+++ previsao_de_ondas.py
@@ -1,35 +1,58 @@
 import requests
+import xml.dom.minidom as minidom
 
-from uteis import obter_codigo_da_cidade
+from previsao_do_tempo import PrevisaoDoTempo
 
 class PrevisaoDeOndas:
     def __init__( self, nome_da_cidade ):
-        self.previsao = ""
-        self.nome_da_cidade = nome_da_cidade
-        self.codigo_da_cidade = obter_codigo_da_cidade( self.nome_da_cidade )
+        self._previsao = ""
+        self._codigo_da_cidade = PrevisaoDoTempo(nome_da_cidade)._obter_codigo_da_cidade()
 
     def _obter_previsao_do_dia( self ):
         
-        if self.codigo_da_cidade:
+        if self._codigo_da_cidade:
 
-            retorno_requisicao = requests.get( "http://servicos.cptec.inpe.br/XML/cidade/" + self.codigo_da_cidade + "/dia/0/ondas.xml" )
+            retorno_requisicao = requests.get( "http://servicos.cptec.inpe.br/XML/cidade/" + self._codigo_da_cidade + "/dia/0/ondas.xml" )
 
-            print("http://servicos.cptec.inpe.br/XML/cidade/" + self.codigo_da_cidade + "/dia/0/ondas.xml")
+            print("http://servicos.cptec.inpe.br/XML/cidade/" + self._codigo_da_cidade + "/dia/0/ondas.xml")
 
             if "undefined" in retorno_requisicao.text:
                 print( "Falha ao obter a previsão!" )
-                return None
+                return ""
 
-            return retorno_requisicao.text
+            return minidom.parseString(retorno_requisicao.text)
 
     def _obter_previsao_da_semana( self ):
         
-        if self.codigo_da_cidade:
+        if self._codigo_da_cidade:
 
-            retorno_requisicao = requests.get( "http://servicos.cptec.inpe.br/XML/cidade/" + self.codigo_da_cidade + "/todos/tempos/ondas.xml" )  
+            retorno_requisicao = requests.get( "http://servicos.cptec.inpe.br/XML/cidade/" + self._codigo_da_cidade + "/todos/tempos/ondas.xml" )  
 
             if not "<previsao>" in retorno_requisicao.text:
                 print( "Falha ao obter a previsão!" )
-                return None
+                return ""
 
-            return retorno_requisicao.text
+            return minidom.parseString(retorno_requisicao.text)
+
+    def _converter_sigla_do_ponto_cardeal(self, sigla):
+
+        previsao = {
+            "N": "Norte",
+            "NNE": "Nor-Nordeste",
+            "NE": "Nordeste",
+            "ENE": "Leste-Nordeste",
+            "E": "Leste",
+            "ESE": "Leste-Sudeste",
+            "SE": "Sudeste",
+            "SSE": "Sul-Sudeste",
+            "S": "Sul",
+            "SSW": "Sul-Sudoeste",
+            "SW": "Sudoeste",
+            "WSW": "Oeste-Sudoeste",
+            "W": "Oeste",
+            "WNW": "Oeste-Noroeste",
+            "NW": "Noroeste",
+            "NNW": "Noroeste"
+        }
+
+        return previsao.get(sigla, "Sigla não encontrada.")
--- view_previsao_de_ondas.py
+++ view_previsao_de_ondas.py
@@ -1,22 +1,76 @@
-import xml.dom.minidom
+from tabulate import tabulate
 from previsao_de_ondas import PrevisaoDeOndas
 
 class ViewPrevisaoDeOndas:
 
     def __init__( self, nome_da_cidade ):
-        self.nome_da_cidade = nome_da_cidade
-        self.previsao_de_ondas = PrevisaoDeOndas( self.nome_da_cidade )
-
+        self._previsao_de_ondas = PrevisaoDeOndas( nome_da_cidade )
+        self._cabecalho = []
+        self._linhas = []
+          
     def exibir_previsao_do_dia(self):
-        previsao_txt = self.previsao_de_ondas._obter_previsao_do_dia()
 
+        previsao_xml = self._previsao_de_ondas._obter_previsao_do_dia()
+        
+        if previsao_xml:
+            
+            cidade = previsao_xml.getElementsByTagName("nome")[0].firstChild.data
+            uf = previsao_xml.getElementsByTagName("uf")[0].firstChild.data
+            atualizacao = previsao_xml.getElementsByTagName("atualizacao")[0].firstChild.data
+            
+            print("Previsao do dia da semana para " + cidade + " - " + uf)
+            print("Atualizada em " + atualizacao)
 
+            self._cabecalho = []
+            self._linhas = []
 
-        return previsao_txt
+            self.extrair_cabecalho(previsao_xml, "manha")
+            self.extrair_lista_de_previsoes(previsao_xml, "manha")
+            self.extrair_lista_de_previsoes(previsao_xml, "tarde")
+            self.extrair_lista_de_previsoes(previsao_xml, "noite")
+
+            print(tabulate(self._linhas, headers=self._cabecalho))
         
     def exibir_previsao_da_semana(self):
-        previsao_txt = self.previsao_de_ondas._obter_previsao_da_semana()
 
+        previsao_xml = self._previsao_de_ondas._obter_previsao_da_semana()
+        
+        if previsao_xml:
+            
+            cidade = previsao_xml.getElementsByTagName("nome")[0].firstChild.data
+            uf = previsao_xml.getElementsByTagName("uf")[0].firstChild.data
+            atualizacao = previsao_xml.getElementsByTagName("atualizacao")[0].firstChild.data
+            
+            print("Previsao de ondas da semana para " + cidade + " - " + uf)
+            print("Atualizada em " + atualizacao)
+            
+            self._cabecalho = []
+            self._linhas = []
 
+            self.extrair_cabecalho(previsao_xml, "previsao")
+            self.extrair_lista_de_previsoes(previsao_xml, "previsao")
 
-        return previsao_txt
+            print(tabulate(self._linhas, headers=self._cabecalho))
+
+    def extrair_cabecalho(self, previsao_xml, nome_da_tag):
+        lista_de_previsoes = previsao_xml.getElementsByTagName(nome_da_tag)
+        for elemento in lista_de_previsoes.item(0).childNodes:
+            if elemento.tagName == "dia":
+                self._cabecalho.append( elemento.tagName.center(18) )
+            else:
+                self._cabecalho.append( elemento.tagName )
+
+    def extrair_lista_de_previsoes(self, previsao_xml, nome_da_tag):
+        lista_de_previsoes = previsao_xml.getElementsByTagName(nome_da_tag)
+        for previsao in lista_de_previsoes:
+            if previsao.hasChildNodes():
+                previsao_filhos = previsao.childNodes
+                linha = []
+                for elemento in previsao_filhos:
+                    if elemento.tagName == "direcao":
+                        linha.append( self._previsao_de_ondas._converter_sigla_do_ponto_cardeal( elemento.firstChild.data ) )
+                    elif elemento.tagName == "vento_dir":
+                        linha.append( self._previsao_de_ondas._converter_sigla_do_ponto_cardeal( elemento.firstChild.data ) )
+                    else:
+                        linha.append( elemento.firstChild.data )
+                self._linhas.append( linha )
